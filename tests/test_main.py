c5aa76cf4e5015c155792803525e8c54d93c2ac8
Paul Sanders <psanders1@gmail.com>
2022-02-25 20:06:19 -0500

    Fix failing test

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,6 +12,7 @@ def test_train_all_models_no_save(mock_data_directory, test_runner, tmp_path):
         "--tags-file-encoding=ISO8859-1",
         f"--naive-bayes-save-path={str(tmp_path)}",
         f"--svm-save-path={str(tmp_path)}",
+        f"--random-forest-save-path={str(tmp_path)}",
         "--test-size=0.01",
     ]
     test_runner.invoke(app, args, catch_exceptions=False)
@@ -31,12 +32,13 @@ def test_train_all_models_save(mock_data_directory, test_runner, tmp_path):
         "--save-model",
         f"--naive-bayes-save-path={str(tmp_path)}",
         f"--svm-save-path={str(tmp_path)}",
+        f"--random-forest-save-path={str(tmp_path)}",
         "--test-size=0.01",
     ]
     result = test_runner.invoke(app, args, catch_exceptions=False)
     result.stdout
 
-    assert len(list(tmp_path.glob("*.pkl"))) == 3
+    assert len(list(tmp_path.glob("*.pkl"))) == 4
 
 
 def test_train_naive_bayes_no_save(mock_data_directory, test_runner, tmp_path):
